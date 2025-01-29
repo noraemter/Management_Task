441d28405377e0999544876f2b33d6029011f41a
noraemter <111689833+noraemter@users.noreply.github.com>
2025-01-29 13:48:15 +0200

    Update task.dart

--- lib/utils/task.dart
+++ lib/utils/task.dart
@@ -1,29 +1,26 @@
 class Task {
-  int? id; // Optional, if you plan to store it in a database
+  int? id; 
   String name;
   bool isCompleted;
 
   Task({this.id, required this.name, this.isCompleted = false});
 
-  // Convert a Task object into a map
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
-      'isCompleted': isCompleted ? 1 : 0, // Store 1 for true, 0 for false
+      'isCompleted': isCompleted ? 1 : 0, 
     };
   }
 
-  // Convert a map into a Task object
   factory Task.fromMap(Map<String, dynamic> map) {
     return Task(
       id: map['id'],
       name: map['name'],
-      isCompleted: map['isCompleted'] == 1, // Convert 1 back to true, 0 to false
+      isCompleted: map['isCompleted'] == 1, 
     );
   }
 
-  // Add the copyWith method
   Task copyWith({int? id, String? name, bool? isCompleted}) {
     return Task(
       id: id ?? this.id,
